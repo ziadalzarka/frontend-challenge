61f6f1521d2dab2ba92e15ca41f28563a2b4e3b1
Ziad Alzarka <ziadalzarka@gmail.com>
2019-12-06 22:34:43 +0200

    feat: i18n

---
+++ layouts/form.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-layout>
+    <v-flex> </v-flex>
+  </v-layout>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -28,13 +28,20 @@ export default {
    */
   css: [],
   /*
+   ** Router custom configuration
+   */
+  router: {
+    middleware: 'i18n'
+  },
+  /*
    ** Plugins to load before mounting the App
    */
   plugins: [
     {
       src: '~/plugins/axe',
       ssr: false
-    }
+    },
+    '~/plugins/i18n'
   ],
   /*
    ** Nuxt.js dev-modules
@@ -64,16 +71,9 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+        light: {
+          primary: colors.teal.base
         }
       }
     }
